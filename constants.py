d9d35a2672c7e31417b7f3837a15f8805661c958
ErrorNoInternet <errornointernet@envs.net>
2025-01-06 10:20:14 -0500

    refactor(constants): sort variables

--- constants.py
+++ constants.py
@@ -1,5 +1,20 @@
 import os
 
+YTDL_OPTIONS = {
+    "color": "never",
+    "default_search": "auto",
+    "format": "bestaudio/best",
+    "ignoreerrors": False,
+    "logtostderr": False,
+    "no_warnings": True,
+    "noplaylist": True,
+    "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
+    "quiet": True,
+    "restrictfilenames": True,
+    "socket_timeout": 15,
+    "source_address": "0.0.0.0",
+}
+
 EMBED_COLOR = 0xFF6600
 OWNERS = [531392146767347712]
 PREFIX = "%"
@@ -18,22 +33,6 @@ RELOADABLE_MODULES = [
     "voice",
     "youtubedl",
 ]
-
-YTDL_OPTIONS = {
-    "color": "never",
-    "default_search": "auto",
-    "format": "bestaudio/best",
-    "ignoreerrors": False,
-    "logtostderr": False,
-    "no_warnings": True,
-    "noplaylist": True,
-    "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
-    "quiet": True,
-    "restrictfilenames": True,
-    "socket_timeout": 15,
-    "source_address": "0.0.0.0",
-}
-
 
 SECRETS = {
     "TOKEN": os.getenv("BOT_TOKEN"),
