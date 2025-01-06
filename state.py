82cd56ace834f8df7a3ffcb88d12bbfdedb302f0
ErrorNoInternet <errornointernet@envs.net>
2025-01-05 19:24:44 -0500

    refactor(state): switch to LimitedSizeDict

--- state.py
+++ state.py
@@ -1,9 +1,27 @@
+import collections
 import time
 
 import disnake
 
-command_locks = {}
-executed_messages = {}
+
+class LimitedSizeDict(collections.OrderedDict):
+    def __init__(self, *args, **kwds):
+        self.size_limit = kwds.pop("size_limit", 1000)
+        super().__init__(*args, **kwds)
+        self._check_size_limit()
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        self._check_size_limit()
+
+    def _check_size_limit(self):
+        if self.size_limit is not None:
+            while len(self) > self.size_limit:
+                self.popitem(last=False)
+
+
+command_locks = LimitedSizeDict()
+message_responses = LimitedSizeDict()
 players = {}
 
 intents = disnake.Intents.default()
