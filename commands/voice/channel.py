d0ddf9ee4781f7d78c720a0186dde72e159b76bb
ErrorNoInternet <errornointernet@envs.net>
2025-01-13 01:08:53 -0500

    fix(commands/voice/join): check if channels correct before joining

--- commands/voice/channel.py
+++ commands/voice/channel.py
@@ -1,3 +1,5 @@
+import disnake
+
 import utils
 
 from .utils import command_allowed
@@ -6,8 +8,14 @@ from .utils import command_allowed
 async def join(message):
     if message.guild.voice_client:
         return await message.guild.voice_client.move_to(message.channel)
+    elif message.author.voice:
+        await message.author.voice.channel.connect()
+    elif isinstance(message.channel, disnake.VoiceChannel):
+        await message.channel.connect()
+    else:
+        await utils.reply(message, "you are not connected to a voice channel!")
+        return
 
-    await message.channel.connect()
     await utils.add_check_reaction(message)
 
 
